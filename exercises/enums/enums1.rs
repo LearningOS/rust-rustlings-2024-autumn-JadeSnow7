41ab50d9cbc2b40a8f0c446a9ed6ba28b738455e
JadeSnow7 <1079249368@qq.com>
2024-10-07 22:20:28 +0800

    solve

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,11 +2,15 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,   // This message is used when the program should exit
+    Echo,   // This message is used to echo a message
+    Move,   // This message is used to move an object
+    ChangeColor,    // This message is used to change the color of an object
 }
 
 fn main() {
